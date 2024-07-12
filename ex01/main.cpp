784e7fe873fe51916e7be810d86453f6342eb3b2
taka-chin <tyamauch@c3r1s3.42tokyo.jp>
2024-07-12 19:36:31 +0900

    fix main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -11,40 +11,52 @@ int main() {
 
   const Cat cat;
   try {
-    cat.strikeOn("i am cat", 101);
-    std::cout << cat.showIdea(0) << std::endl;
+    cat.strikeOn("i am cat", 0);
 
     const Cat copycat = cat;
     const Cat copycat1(cat);
-    std::cout << copycat.showIdea(0) << std::endl;
-    std::cout << copycat1.showIdea(0) << std::endl;
 
-    cat.strikeOn("i am dog", 0);
+    std::cout << "==============================" << std::endl;
     std::cout << cat.showIdea(0) << std::endl;
     std::cout << copycat.showIdea(0) << std::endl;
     std::cout << copycat1.showIdea(0) << std::endl;
+    std::cout << "==============================" << std::endl;
+
+    cat.strikeOn("i am dog", 0);
+
+    std::cout << "==============================" << std::endl;
+    std::cout << cat.showIdea(0) << std::endl;
+    std::cout << copycat.showIdea(0) << std::endl;
+    std::cout << copycat1.showIdea(0) << std::endl;
+    std::cout << "==============================" << std::endl;
   } catch (const std::out_of_range &e) {
     std::cerr << "Exception: " << e.what() << std::endl;
     return (1);
   }
+    std::cout << "==============================" << std::endl;
 
   while (i < N) {
     if (i < N / 2) {
+    std::cout << "==============================" << std::endl;
       std::cout << i << std::endl;
       animals[i] = new Dog();
       animals[i]->makeSound();
     } else {
+    std::cout << "==============================" << std::endl;
       std::cout << i << std::endl;
       animals[i] = new Cat();
       animals[i]->makeSound();
     }
     i++;
   }
+    std::cout << "==============================" << std::endl;
   i = 0;
   while (i < N) {
+    std::cout << "==============================" << std::endl;
     std::cout << i << std::endl;
     delete animals[i];
     i++;
   }
+    std::cout << "==============================" << std::endl;
   return 0;
 }
