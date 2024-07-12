c32a7793447c6d7426903b5a03b9ff3a0c5ab9e7
taka-chin <tyamauch@c3r1s3.42tokyo.jp>
2024-07-12 19:51:50 +0900

    fix

--- ex02/Brain.cpp
+++ ex02/Brain.cpp
@@ -5,17 +5,37 @@ Brain::Brain() { std::cout << "Brain Default constructor called" << std::endl; }
 
 Brain::~Brain() { std::cout << "Brain destructor called" << std::endl; }
 
-Brain::Brain(const Brain &f) {
+Brain::Brain(const Brain &other) {
   std::cout << "Brain Copy constructor called" << std::endl;
-  *this = f;
+  *this = other;
 }
 
-Brain &Brain::operator=(const Brain &f) {
+Brain &Brain::operator=(const Brain &other) {
   std::cout << "Brain Copy assignment operator called" << std::endl;
-  int i = 0;
-  while (i < 100) {
-    this->idea[i] = f.idea[i];
+  size_t i = 0;
+  if (this != &other) {
+  while (i < count) {
+    this->idea[i] = other.idea[i];
     i++;
   }
+  }
   return *this;
+}
+
+size_t Brain::getCount() { return count; }
+
+std::string Brain::getIdea(size_t index) {
+  if (index >= count) {
+    throw std::out_of_range("Index out of range");
+  }
+  std::cout << "Brain get idea function called" << std::endl;
+  return this->idea[index];
+}
+
+void Brain::setIdea(const std::string &something, size_t index) {
+  if (index >= count) {
+    throw std::out_of_range("Index out of range");
+  }
+  std::cout << "Brain set idea function called" << std::endl;
+  this->idea[index] = something;
 }
--- ex02/Brain.hpp
+++ ex02/Brain.hpp
@@ -1,17 +1,22 @@
 #ifndef BRAIN_HPP
 #define BRAIN_HPP
 
+#include <cstddef>
 #include <string>
 
 class Brain {
 private:
-  std::string idea[100];
+  static const size_t count = 100;
+  std::string idea[count];
 
 public:
   Brain();
   virtual ~Brain();
-  Brain(const Brain &f);
-  Brain &operator=(const Brain &f);
+  Brain(const Brain &other);
+  Brain &operator=(const Brain &other);
+  std::string getIdea(size_t index);
+  void setIdea(const std::string &something, size_t index);
+  size_t getCount();
 };
 
 #endif
